54018d4899f940c782a2738a082c1d5960352657
NUAA-YANG <2997357856@qq.com>
2023-04-27 10:38:07 +0800

    项目初始化

---
+++ src/main/java/com/nuaa/automaticallygeneratenetwork/AutomaticallyGenerateNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.nuaa.automaticallygeneratenetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticallyGenerateNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticallyGenerateNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nuaa/automaticallygeneratenetwork/AutomaticallyGenerateNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuaa.automaticallygeneratenetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticallyGenerateNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
